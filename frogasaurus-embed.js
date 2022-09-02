e0941aa8e5c9de966b7f3812ebc7d0b21b15cd4e
Luke Wilson <l2wilson94@gmail.com>
2022-09-02 15:11:06 +0100

    fix something

---
+++ frogasaurus-embed.js
@@ -0,0 +1,39 @@
+//=============//
+// FROGASAURUS //
+//=============//
+const FrogasaurusFrogasaurus = {}
+
+//========//
+// SOURCE //
+//========//
+{
+	//====== ./greet.js ======
+	{
+		FrogasaurusFrogasaurus["./greet.js"] = {}
+		const greet = (name) => console.log(`Hello ${name}!`)
+
+		FrogasaurusFrogasaurus["./greet.js"].greet = greet
+	}
+
+	//====== ./hello.js ======
+	{
+		FrogasaurusFrogasaurus["./hello.js"] = {}
+		
+		const main = (name = "world") => {
+			greet(name)
+		}
+
+		FrogasaurusFrogasaurus["./hello.js"].main = main
+	}
+
+	const { greet } = FrogasaurusFrogasaurus["./greet.js"]
+
+}
+
+//=========//
+// EXPORTS //
+//=========//
+const Frogasaurus = {
+	greet: FrogasaurusFrogasaurus["./greet.js"].greet,
+	main: FrogasaurusFrogasaurus["./hello.js"].main,
+}
